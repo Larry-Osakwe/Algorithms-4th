df742df1e7de2ff41ec99296f9948aba816c4a22
Larry <larryosak@gmail.com>
2017-08-01 17:13:42 -0700

    ex 1.3.4

--- src/algorithms/CH1_Fundamentals/P1_3_BagsQueuesStacks/EX_1_3_4.java
+++ src/algorithms/CH1_Fundamentals/P1_3_BagsQueuesStacks/EX_1_3_4.java
@@ -5,10 +5,59 @@
  */
 package algorithms.CH1_Fundamentals.P1_3_BagsQueuesStacks;
 
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+
+
 /**
  *
  * @author Larry Osakwe
  */
 public class EX_1_3_4 {
+    private static final char LEFT_BRACKET = '[';
+    private static final char LEFT_PARENTHESES = '(';
+    private static final char LEFT_CURLYBRACKET = '{';
+    private static final char RIGHT_BRACKET = ']';
+    private static final char RIGHT_PARENTHESES = ')';
+    private static final char RIGHT_CURLYBRACKET = '}';
+    
+    public static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == LEFT_BRACKET) stack.push(LEFT_BRACKET);
+            if (s.charAt(i) == LEFT_PARENTHESES) stack.push(LEFT_PARENTHESES);
+            if (s.charAt(i) == LEFT_CURLYBRACKET) stack.push(LEFT_CURLYBRACKET);
+            
+            if (s.charAt(i) == RIGHT_BRACKET)
+                if (stack.isEmpty()) return false;
+                else if (stack.pop() != LEFT_BRACKET) {
+                    return false;
+                }
+            if (s.charAt(i) == RIGHT_PARENTHESES)
+                if (stack.isEmpty()) return false;
+                else if (stack.pop() != LEFT_PARENTHESES) {
+                    return false;
+                }
+            if (s.charAt(i) == RIGHT_CURLYBRACKET)
+                if (stack.isEmpty()) return false;
+                else if (stack.pop() != LEFT_CURLYBRACKET) {
+                    return false;
+                }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    public static void main(String[] args) {
+        String f = "{([][])}()()";
+        StdOut.println(isBalanced(f));
+        
+        String k = "}(){";
+        StdOut.println(isBalanced(k));
+    }
+    
+    
+    
     
 }
