3d0be64efae199227f7b7aad031204b872cfc7bb
Larry <larryosak@gmail.com>
2017-08-04 16:30:34 -0700

    1.3.14 ResizingArrayQueueOfStrings

--- src/algorithms/CH1_Fundamentals/P1_3_BagsQueuesStacks/ResizingArrayQueueOfStrings.java
+++ src/algorithms/CH1_Fundamentals/P1_3_BagsQueuesStacks/ResizingArrayQueueOfStrings.java
@@ -5,10 +5,68 @@
  */
 package algorithms.CH1_Fundamentals.P1_3_BagsQueuesStacks;
 
+import java.util.NoSuchElementException;
+
 /**
  *
  * @author Larry Osakwe
  */
 public class ResizingArrayQueueOfStrings {
+
+    private String[] q;
+    private int N;
+    private int first;
+    private int last;
+
+    public ResizingArrayQueueOfStrings() {
+        q = new String[2];
+        N = 0;
+        first = 0;
+        last = 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    private void resize(int capacity) {
+        String[] temp = new String[capacity];
+        for (int i = 0; i < N; i++) {
+            temp[i] = q[(first + 1) % q.length];
+        }
+        q = temp;
+        first = 0;
+        last = N;
+    }
     
+    public void enqueue(String s) {
+        if (q.length == N) {
+            resize(2*q.length);
+        }
+        q[last++] = s;
+        if (last == q.length) {
+            last = 0;
+        }
+        N++;
+    }
+    
+    public String dequeue() {
+        if (isEmpty()) throw new NoSuchElementException();
+        String string = q[first];
+        q[first] = null;
+        N--;
+        first++;
+        if (first == q.length) {
+            first = 0;
+        }
+        if (N>0 && N == q.length/4) {
+            resize (q.length/2);
+        }
+        return string;
+    }
+
 }
